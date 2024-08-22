f554fdf0e0d922f400042391bbedf4838d9d5e33
Piyush Prajapati <piyushoa2004@gmail.com>
2024-08-22 20:49:34 +0530

    task4 complete

--- app/src/main/java/com/piyushprajpti/cognifyzproject/screens/Task4Screen.kt
+++ app/src/main/java/com/piyushprajpti/cognifyzproject/screens/Task4Screen.kt
@@ -1,9 +1,26 @@
 package com.piyushprajpti.cognifyzproject.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.piyushprajpti.cognifyzproject.util.TopBar
 
 @Composable
 fun Task4Screen(onBackClick: () -> Unit) {
-    
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopBar(onBackClick = onBackClick)
+
+        Text(
+            text = """"I have completed the task of building an app with multiple activities and a basic UI layout using Jetpack Compose instead of XML layout."""",
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(start = 20.dp, top = 150.dp, end = 20.dp, bottom = 0.dp)
+        )
+    }
 }

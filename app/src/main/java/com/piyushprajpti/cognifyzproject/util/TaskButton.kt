8e0d33579ab8e1cbe293a26bb192c9c3422146d2
Piyush Prajapati <piyushoa2004@gmail.com>
2024-08-22 18:29:21 +0530

    reusable button created

---
+++ app/src/main/java/com/piyushprajpti/cognifyzproject/util/TaskButton.kt
@@ -0,0 +1,35 @@
+package com.piyushprajpti.cognifyzproject.util
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.piyushprajpti.cognifyzproject.ui.theme.PrimaryColor
+
+@Composable
+fun TaskButton(
+    title: String,
+    onClick: () -> Unit
+) {
+    Text(
+        text = title,
+        fontSize = 20.sp,
+        textAlign = TextAlign.Center,
+        color = Color.Black,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(PrimaryColor, CircleShape)
+            .clip(CircleShape)
+            .clickable { onClick() }
+            .padding(vertical = 15.dp)
+    )
+}
